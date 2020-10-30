c304d4a090ef599b3fa7b9b578a13f536f13b893
Hami Hafezi <dev.hamihafezi@gmail.com>
2020-10-30 19:36:52 +0330

    first commit

---
+++ app/src/main/java/com/hfz101/gitlearnings/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hfz101.gitlearnings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
